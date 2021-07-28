35bda805a9ccaf65d5c1f15c0545f9abd36ecaf7
akinrepostion <odekuakinlolu@gmail.com>
2021-07-28 13:08:54 +0100

    checking of the route

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { PrivateRoute } from './Routes/PrivateRoute.js';
 import { PublicRoute } from './Routes/PublicRoute.js'
 import { Provider } from 'react-redux';
-import { AuthContextProvider } from './authentication/AuthContext.js';
+// import { AuthContextProvider } from './authentication/AuthContext.js';
 import store from './store'
 import routes from './routes';
+// import LandingPage from './pages/LandingPage';
 import NotFound from './pages/NotFound'
 import './App.scss';
+// import LocalGossips from './pages/LocalGossips';
+// import LCDA from './pages/LCDAs';
+// import ContactUs from './pages/ContactUs';
 
 const App = () => {
   return(
     <Provider store={store}>
-      <AuthContextProvider>
-        <Router>
+      <Router>
             <Switch>
                 {routes.map((route, i) =>{
                   if(route.isPrivate){
@@ -22,10 +25,13 @@ const App = () => {
                     return <PublicRoute key={i} {...route}/>
                   }
                 })}
+                {/* <Route exact path='/' component={LandingPage}/>
+                <Route exact path='/news' component={LocalGossips}/>
+                <Route exact path="/lcda" component={LCDA}/>
+                <Route exact path="/contact-us" component={ContactUs}/> */}
                 <Route component={NotFound} />
             </Switch>
         </Router>
-      </AuthContextProvider>
     </Provider>
   )
 }
